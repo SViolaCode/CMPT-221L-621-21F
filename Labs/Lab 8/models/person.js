19b1d25503ec7b1cb3b1051b2fa602e19f06afc9
SViolaCode <Samuel.Viola1@marist.edu>
2021-11-17 22:04:09 -0500

    Delete person.js

--- Labs/Lab 8/models/person.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
-})
-module.exports = mongoose.model("Person", personSchema);
